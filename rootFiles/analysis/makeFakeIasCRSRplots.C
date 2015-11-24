f26123dab3f48ec94a20d594883d1b9e9005668a
Teresa Lenz <teresa.lenz@desy.de>
2015-11-24 16:28:10 +0100

    new

--- rootFiles/analysis/makeFakeIasCRSRplots.C
+++ rootFiles/analysis/makeFakeIasCRSRplots.C
@@ -22,15 +22,9 @@
 #include <cmath>
 #include <algorithm>
 #include <iomanip>
-#include "../plotStyleThesis.h"
-
-
-TCanvas *drawRatioPlotWithPurity(TH1D *prediction, TH1D *sr, TString xTitle, TString filename, double purity, double ptcut);
-void getPoissonIntervalls(double mu1, double mu2);
+#include "drawRatioPlot.h"
 
 class sample{
-
-
 public:
 
   TFile *file;
@@ -207,9 +201,8 @@ public:
 };
 
 
-int makeFakeIasCRSRplots(){
+int makeFakeIasCRSRplots(double ptCut=20){
 
-  double ptCut=40;
   double ecaloCut=5;
   double iasCut=0.2;
 
@@ -259,20 +252,55 @@ int makeFakeIasCRSRplots(){
   SR.histoASmiSR  -> Scale(1./SR.histoASmiSR->Integral());
   SR.histoASmi    -> Scale(1./SR.histoASmi->Integral());
 
-
-  TeresaPlottingStyle::init();
-  gROOT->ForceStyle();
   TString filename = "";
-
   filename = "hASmi_SRbinning_fakes_";
   filename += Form("ECalaoLe%.0f_",ecaloCut);
-  filename += Form("trackPtGt%.0f_",ptCut);
-  TCanvas *c1 = drawRatioPlotWithPurity(pred.histoASmiSR,SR.histoASmiSR,"dE/dx discriminator (I_{as})",filename + pred.histoASmi->GetName() + "_"  + SR.histoASmi->GetName() + ".pdf" ,pred.purity,ptCut);
+  filename += Form("trackPtGt%.0f",ptCut);
+  TCanvas *c = drawRatioPlot(pred.histoASmiSR,SR.histoASmiSR,"dE/dx discriminator (I_{as})","N_{SR}/N_{CR}","simulated events from CR_{I_{as}}^{fake}","simulated events from SR");
+  c->cd();
+  TPad* pad = (TPad*) gPad->GetPrimitive("pad1");
+
+  pad->cd();
+  TLatex*  info   = new TLatex();
+  info-> SetNDC();
+  info->SetTextSize(0.06);
+  TString AuxString = Form("Purity of fakes in CR = %4.1f ",pred.purity*100);
+  info->DrawLatex(0.43,0.60,AuxString);
+  TLatex*  info1   = new TLatex();
+  info1-> SetNDC();
+  info1->SetTextSize(0.06);
+  AuxString.Form("p_{T}>%4.0fGeV",ptCut);
+  info1->DrawLatex(0.43,0.50,AuxString);
+  c->SaveAs(filename + ".pdf");
+
+
+  filename = "hASmi_fakes_";
+  filename += Form("ECalaoLe%.0f_",ecaloCut);
+  filename += Form("trackPtGt%.0f",ptCut);
+  TCanvas *c1 = drawRatioPlot(pred.histoASmi,SR.histoASmi,"dE/dx discriminator (I_{as})","N_{CR}/N_{SR}","simulated events from CR_{I_{as}}^{fake}","simulated events from SR");
+  c1->cd();
+  pad = (TPad*) gPad->GetPrimitive("pad1");
+
+  pad->cd();
+  info   = new TLatex();
+  info-> SetNDC();
+  info->SetTextSize(0.06);
+  AuxString = Form("Purity of fakes in CR = %4.1f ",pred.purity*100);
+  info->DrawLatex(0.43,0.60,AuxString);
+  info1   = new TLatex();
+  info1-> SetNDC();
+  info1->SetTextSize(0.06);
+  AuxString.Form("p_{T}>%4.0fGeV",ptCut);
+  info1->DrawLatex(0.43,0.50,AuxString);
+  c1->SaveAs(filename + ".pdf");
+
+
+  /*
   filename = "hASmi_fakes_";
   filename += Form("ECalaoLe%.0f_",ecaloCut);
   filename += Form("trackPtGt%.0f_",ptCut);
-  TCanvas *c2 = drawRatioPlotWithPurity(pred.histoASmi,SR.histoASmi,"dE/dx discriminator (I_{as})",filename + pred.histoASmi->GetName() + "_"  + SR.histoASmi->GetName() + ".pdf" ,pred.purity,ptCut);
- 
+  drawRatioPlotWithPurity(pred.histoASmi,SR.histoASmi,"dE/dx discriminator (I_{as})",filename + pred.histoASmi->GetName() + "_"  + SR.histoASmi->GetName() + ".pdf" ,pred.purity,ptCut);
+  */
   return 0;
  
 }
@@ -281,149 +309,4 @@ int makeFakeIasCRSRplots(){
 //################################################################################################################################
 //################################################################################################################################
 
-
 //################################################################################################################################
- // Get the 68% confindence interval of mu1/mu2
-void getPoissonIntervalls(double mu1, double mu2){
-
-  double result = mu1/mu2;
-  TH1D *expHist = new TH1D("mu1/mu2","mu1/mu2",1000,0,1);
-  TRandom3 rand1(0);
-  TRandom3 rand2(0);
-
-  cout<<"mu1 = "<<mu1<<endl;
-  cout<<"mu2 = "<<mu2<<endl;
-  cout<<"ratio = "<<result<<endl;
-
-  for(int i=0; i<10000000; i++){
-
-    expHist->Fill(1.*rand1.Poisson(mu1)/rand2.Poisson(mu2));
-  }
-
-  // Get now the 68% upper and lower interval
-  double errUp  = 0;
-  double errLow = 0;
-  for(int i=1; i<=expHist->GetNbinsX();i++){
-
-    double upperIntegral = expHist->Integral(i,expHist->GetNbinsX());
-    double lowerIntegral = expHist->Integral(1,i);
-
-    if(abs(upperIntegral/expHist->Integral()-0.32)<0.02){
-      cout<<"upper bound = "<<expHist->GetBinCenter(i)<<endl;
-      errUp = expHist->GetBinCenter(i)-result;
-    }
-    if(abs(lowerIntegral/expHist->Integral()-0.32)<0.02){
-      cout<<"lower bound = "<<expHist->GetBinCenter(i)<<endl;
-      errLow = result - expHist->GetBinCenter(i);
-    }
-  }
-
-
-  cout<<"error Up = "<<errUp<<endl;
-  cout<<"error Low = "<<errLow<<endl;
-
-  TCanvas *c = new TCanvas("cExp","cExp",0,0,500,500);
-  expHist->Draw();
-}    
-//################################################################################################################################
-TCanvas *drawRatioPlotWithPurity(TH1D *prediction, TH1D *sr, TString xTitle, TString filename, double purity, double ptcut){
-  
-  TeresaPlottingStyle::init();
-
-  gStyle->SetMarkerSize(1.0);
-  gStyle->SetNdivisions(502,"X");
-  gStyle->SetNdivisions(509,"Y");
-
-  TCanvas *c = new TCanvas();
-
-  float y = 0.32;
-
-  TPad *pad1     = new TPad("pad1", "Control Plots 1", 0.01, y, 0.99, 0.99);
-  TPad *padRatio = new TPad("rp1", "Ratio1", 0.01, 0.01, 0.99, y-0.02);
-
-  TH1D *ratio = 0;
-
-  ratio = (TH1D*) sr->Clone();
-  ratio->Divide(prediction);
-  ratio->GetYaxis()->SetTitle((TString) sr->GetTitle() + "/" + (TString) prediction->GetTitle());
-  ratio->SetLineColor(kBlack);
-  ratio->SetMarkerColor(kBlack);
-
-
-  padRatio->cd();
-  ratio->GetYaxis()->SetRangeUser(0.5,1.5);
-  ratio->SetTitleSize(0.15,"XYZ");
-  ratio->SetLabelSize(0.13,"XYZ");
-  ratio->GetYaxis()->SetTitleOffset(0.5);
-  ratio->GetYaxis()->SetTitle("N_{SR}/N_{CR}");
-  ratio->GetYaxis()->CenterTitle();
-  ratio->GetYaxis()->SetNdivisions(505);
-  ratio->Draw();
-
-  // Draw line at one!
-  TLine *line = new TLine(0,1,1,1);
-  line->SetLineWidth(2);
-  line->Draw("same");
-  padRatio->Modified();
-
-
-  TLegend *leg = new TLegend(0.30,0.75,0.85,0.9);
-  leg->SetTextSize(0.06);
-  leg->AddEntry(sr,"simulated events from SR","pel"); 
-  leg->AddEntry(prediction,"simulated events from CR_{I_{as}}^{fake}","pel"); 
-  pad1->cd();
-  pad1->SetLogy();
-
-  ratio      -> GetXaxis()->SetNdivisions(505);
-  prediction -> GetXaxis()->SetNdivisions(505);
-
-  sr->SetLineColor(kRed);
-  sr->SetMarkerColor(kRed);
-  sr->SetMarkerStyle(20);
-  prediction->SetLineColor(kBlack);
-  prediction->SetMarkerColor(kBlack);
-  prediction->SetMarkerStyle(20);
-
-
-  prediction->GetYaxis()->SetTitleOffset(0.7);
-  prediction->GetXaxis()->SetTitleOffset(0.95);
-  prediction->SetTitleSize(0.08,"XYZ");
-  prediction->SetLabelSize(0.06,"XYZ");
-  prediction->GetYaxis()->SetTitle("a.u.");
-  prediction->GetXaxis()->SetTitle("dE/dx discriminator (I_{as})");
-  prediction->Draw("e");
-  sr->Draw("e same");
-
-
-  prediction->SetMaximum(5);
-  prediction->SetMinimum(0.05);
-  leg->Draw("same");
-
-
-  //***********************
-  TLatex*  info   = new TLatex();
-  info-> SetNDC();
-  info->SetTextSize(0.06);
-  TString AuxString = Form("Purity of fakes in CR = %4.1f ",purity*100);
-  info->DrawLatex(0.43,0.60,AuxString);
-
-  TLatex*  info1   = new TLatex();
-  info1-> SetNDC();
-  info1->SetTextSize(0.06);
-  AuxString.Form("p_{T}>%4.0fGeV",ptcut);
-  info1->DrawLatex(0.43,0.50,AuxString);
-  //***********************
-
-  // Draw both pads to canvas
-  c->cd();
-  pad1->Draw();
-  padRatio->SetGridy();
-  c -> SetBottomMargin(0.55);
-  //c->Modified();
-  padRatio->Draw();
-
-  c->SaveAs(filename);
-
-  return c;
-
-}
