be35fa1a9e435f0ebe08917faf7335661bdeb9e8
Teresa Lenz <teresa.lenz@desy.de>
2015-12-03 17:55:04 +0100

    update 3.12.2015, 18:00

--- rootFiles/analysis/AnalysisSelection/makeCutflowTable.C
+++ rootFiles/analysis/AnalysisSelection/makeCutflowTable.C
@@ -19,7 +19,7 @@ int makeCutflowTable()
 
   //TString pathName="/afs/desy.de/user/t/tlenz//xxl-af-cms/ANALYSIS/workdir/analysis_2015_11_18_METGt100_JetPt110_TrackPtGt20/results/analyzer/ntuples/input_weighted";
   //TString pathName="~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_11_12_METGt100_JetPt110/results/analyzer/ntuples/input_weighted";
-  TString pathName="~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_11_30_METGt100_JetPt110_TrackPtGt20/results/analyzer/ntuples/input_weighted";
+  TString pathName="~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_12_02_METGt100_JetPt110_TrackPtGt20/results/analyzer/ntuples/input_weighted";
   TFile *file1 = TFile::Open(pathName + "/wjets.root","READ");
   file1->GetObject("chiTracksfullSelectionTrigger/countsTrackCriteria",hCutflow);
   //samples.push_back(hCutflow);
@@ -67,24 +67,27 @@ int makeCutflowTable()
     if(i==4)  cout<<left<<setw(90)<<"$\\met>100\\gev$";
     if(i==5)  cout<<left<<setw(90)<<"$\\Delta\\phi_{\\text{max}} \\left( \\text{jet}_i, \\text{jet}_j  \\right)<2.7$";
     if(i==6)  cout<<left<<setw(90)<<"$\\Delta\\phi_{\\text{max}} \\left( \\text{jet}_i, \\met  \\right)>0.5$";
-    if(i==7)  cout<<left<<setw(90)<<"$\\geq1$ reconstructed trk";
-    if(i==8)  cout<<left<<setw(90)<<"$\\geq1$ high-purity trk";
-    if(i==9)  cout<<left<<setw(90)<<"$\\geq1$ trk with $N_{\\text{miss}}^{\\text{middle}}=0$";
-    if(i==10) cout<<left<<setw(90)<<"$\\geq1$ trk with $N_{\\text{miss}}^{\\text{inner}}=0$";
-    if(i==11) cout<<left<<setw(90)<<"$\\geq1$ trk with $|d0|<0.02\\cm$";
-    if(i==12) cout<<left<<setw(90)<<"$\\geq1$ trk with $|dz|<0.5\\cm$";
-    if(i==13) cout<<left<<setw(90)<<"$\\geq1$ trk with $|\\eta|<2.1$";
-    if(i==14) cout<<left<<setw(90)<<"$\\geq1$ trk with $\\pt>20\\gev$";
-    if(i==15) cout<<left<<setw(90)<<"$\\geq1$ trk without a $\\mu$ within $\\Delta R<0.15$";
-    if(i==16) cout<<left<<setw(90)<<"$\\geq1$ trk without an e within $\\Delta R<0.15$";
-    if(i==17) cout<<left<<setw(90)<<"$\\geq1$ trk without a $\\tau$ within $\\Delta R<0.15$";
-    if(i==18) cout<<left<<setw(90)<<"$\\geq1$ trk without a jet within $\\Delta R<0.5$";
-    if(i==19) cout<<left<<setw(90)<<"\\makecell[l]{$\\geq1$ trk ! within $\\Delta R<0.05$ of \\\\\\hfill a dead/noisy ECAL cell}";
-    if(i==20) cout<<left<<setw(90)<<"\\makecell[l]{$\\geq1$ trk ! within an ECAL  intermodule gap}";
-    if(i==21) cout<<left<<setw(90)<<"$\\geq1$ trk ! within $1.42<|\\eta|<1.65$";
-    if(i==22) cout<<left<<setw(90)<<"$\\geq1$ trk ! within $\\Delta R<0.25$ to a bad CSC";
-    if(i==23) cout<<left<<setw(90)<<"$\\geq1$ trk with $\\sum \\limits_{\\Delta R < 0.3} \\pt/p_{\\text{T}}^{\\text{cand}} < 0.1$";
-    if(i==24) cout<<left<<setw(90)<<"$\\geq1$ trk with $\\ecalo<5\\gev$";
+    if(i==7){
+      cout<<left<<setw(90)<<"$\\geq1$ track in the event with:"<<endl;
+      cout<<left<<setw(90)<<"reconstructed trk";
+    }
+    if(i==8)  cout<<left<<setw(90)<<"high-purity";
+    if(i==9)  cout<<left<<setw(90)<<"$N_{\\text{miss}}^{\\text{middle}}=0$";
+    if(i==10) cout<<left<<setw(90)<<"$N_{\\text{miss}}^{\\text{inner}}=0$";
+    if(i==11) cout<<left<<setw(90)<<"$|d0|<0.02\\cm$";
+    if(i==12) cout<<left<<setw(90)<<"$|dz|<0.5\\cm$";
+    if(i==13) cout<<left<<setw(90)<<"$|\\eta|<2.1$";
+    if(i==14) cout<<left<<setw(90)<<"$\\pt>20\\gev$";
+    if(i==15) cout<<left<<setw(90)<<"No $\\mu$ within $\\Delta R<0.15$";
+    if(i==16) cout<<left<<setw(90)<<"No e within $\\Delta R<0.15$";
+    if(i==17) cout<<left<<setw(90)<<"No $\\tau$ within $\\Delta R<0.15$";
+    if(i==18) cout<<left<<setw(90)<<"No jet within $\\Delta R<0.5$";
+    if(i==19) cout<<left<<setw(90)<<"\\makecell[l]{Not within $\\Delta R<0.05$ of \\\\\\hfill a dead/noisy ECAL cell}";
+    if(i==20) cout<<left<<setw(90)<<"Not within an ECAL  intermodule gap";
+    if(i==21) cout<<left<<setw(90)<<"Not within $1.42<|\\eta|<1.65$";
+    if(i==22) cout<<left<<setw(90)<<"Not within $\\Delta R<0.25$ to a bad CSC";
+    if(i==23) cout<<left<<setw(90)<<"$\\sum \\limits_{\\Delta R < 0.3} \\pt/p_{\\text{T}}^{\\text{cand}} < 0.1$";
+    if(i==24) cout<<left<<setw(90)<<"$\\ecalo<5\\gev$";
     
     /*
     if(i==8)  cout<<left<<setw(90)<<"high-purity";
