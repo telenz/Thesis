65a55da5d47435c5a1c4bc5f777f05de93950451
Teresa Lenz <teresa.lenz@desy.de>
2015-12-04 20:16:13 +0100

    update 4.12.2015, 20:30

---
+++ rootFiles/analysis/Validation/SignalContamination.C
@@ -0,0 +1,215 @@
+#include "TROOT.h"
+#include "TFile.h"
+#include "TProfile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TMath.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TString.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TLine.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TRandom3.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+
+class sample{
+public:
+
+  TFile *file;
+  TTree *tree;
+
+  TH1D* histo;
+
+  int    statistics;
+  double purity;
+
+  vector<double> *trackEta;
+  vector<double> *trackPt;
+  vector<double> *trackP;
+  vector<double> *trackGenPt;
+  vector<double> *trackASmi;
+  vector<double> *trackHarm2;
+  vector<double> *trackCaloIso;
+  vector<double> *trackMass;
+  vector<double> *trackIsolation;
+  vector<double> *trackGenE;
+  vector<double> *trackGenEt;
+  vector<double> *trackEndVertexRho ;
+  vector<double> *trackChi2;
+  vector<double> *trackNdof;
+  vector<double> *trackHCALRp5Isolation;
+  vector<double> *trackECALRp5Isolation;
+  vector<double> *trackHCALRp4Isolation;
+  vector<double> *trackECALRp4Isolation;
+  vector<double> *trackHCALRp3Isolation;
+  vector<double> *trackECALRp3Isolation;
+  vector<int>    *trackNLostOuter;
+  vector<int>    *trackNLostMiddle;
+  vector<int>    *trackNLostInner;
+  vector<int>    *trackNValid;
+  vector<int>    *trackPdgId;
+  double weight;
+  double weight_xsec_lumi;
+  double met;
+  double leadingJetPt;
+
+  vector<double> *trackDeDx1;
+  vector<double> *trackDeDx2;
+  vector<double> *trackDeDx3;
+  vector<double> *trackDeDx4;
+  vector<double> *trackDx1;
+  vector<double> *trackDx2;
+  vector<double> *trackDx3;
+  vector<double> *trackDx4;
+
+  vector<int>    *trackMeasSize;
+
+  sample(){
+
+    tree=0;
+    file=0;
+    histo=0;
+    statistics=0;
+    purity=0;
+
+    trackEta        = 0;
+    trackEta        = 0;
+    trackPt         = 0;
+    trackP          = 0;
+    trackGenPt      = 0;
+    trackASmi       = 0;
+    trackHarm2      = 0;
+    trackCaloIso    = 0;
+    trackMass       = 0;
+    trackIsolation  = 0;
+    trackGenE       = 0;
+    trackGenEt      = 0;
+    trackEndVertexRho = 0;
+    trackChi2 = 0;
+    trackNdof = 0;
+    trackHCALRp5Isolation = 0;
+    trackECALRp5Isolation = 0;
+    trackHCALRp4Isolation = 0;
+    trackECALRp4Isolation = 0;
+    trackHCALRp3Isolation = 0;
+    trackECALRp3Isolation = 0;
+    trackNLostOuter = 0;
+    trackNLostMiddle = 0;
+    trackNLostInner = 0;
+    trackNValid     = 0;
+    trackPdgId      = 0;
+    trackDeDx1   = 0;
+    trackDeDx2   = 0;
+    trackDeDx3   = 0;
+    trackDeDx4   = 0;
+    trackDx1   = 0;
+    trackDx2   = 0;
+    trackDx3   = 0;
+    trackDx4   = 0;
+    trackMeasSize= 0;
+  };
+
+  void getTreeVariables(){
+    tree->SetBranchAddress("trackEta",&trackEta);
+    tree->SetBranchAddress("trackPt",&trackPt);
+    tree->SetBranchAddress("trackP",&trackP);
+    tree->SetBranchAddress("trackGenPt",&trackGenPt);
+    tree->SetBranchAddress("trackDeDxASmi",&trackASmi);
+    tree->SetBranchAddress("trackDeDxHarm2",&trackHarm2);
+    tree->SetBranchAddress("trackCaloIsolation",&trackCaloIso);
+    tree->SetBranchAddress("trackMass",&trackMass);
+    tree->SetBranchAddress("trackIsolation",&trackIsolation);
+    tree->SetBranchAddress("trackGenE",&trackGenE);
+    tree->SetBranchAddress("trackGenEt",&trackGenEt);
+    tree->SetBranchAddress("trackEndVertexRho",&trackEndVertexRho);
+    tree->SetBranchAddress("trackChi2",&trackChi2);
+    tree->SetBranchAddress("trackNdof",&trackNdof);
+    tree->SetBranchAddress("trackNLostOuter",&trackNLostOuter);
+    tree->SetBranchAddress("trackNLostInner",&trackNLostInner);
+    tree->SetBranchAddress("trackNLostMiddle",&trackNLostMiddle);
+    tree->SetBranchAddress("trackNValid",&trackNValid);
+    tree->SetBranchAddress("trackPdgId",&trackPdgId);
+    tree->SetBranchAddress("MET",&met);
+    tree->SetBranchAddress("LeadingJetPt",&leadingJetPt);
+    tree->SetBranchAddress("weight",&weight);
+    tree->SetBranchAddress("weight_xsec_lumi",&weight_xsec_lumi);
+  };
+
+
+  void Selection(){
+
+    histo->Sumw2();
+    histo->SetTitle("");
+
+    statistics = 0;
+    purity=0;
+
+    for(int n=0; n<tree->GetEntries(); n++){
+
+      tree->GetEntry(n);
+
+      if(met<100)                        continue;
+      if(leadingJetPt<110)               continue;
+
+
+      for(unsigned int i=0; i<trackEta->size(); i++){
+
+	if(abs(trackEta->at(i))>2.1)                                 continue;
+	if(abs(trackEta->at(i))>1.42 && abs(trackEta->at(i))<1.65 )  continue;
+	if(trackASmi->at(i)<0.2)           continue;
+    
+	if(trackPt->at(i)<20)          continue;
+	if(trackCaloIso->at(i)<10)  continue;
+	
+
+	if(trackNValid->at(i)<7) continue;
+	
+      
+	histo         -> Fill(trackASmi->at(i),weight_xsec_lumi*weight);
+      }
+    }
+
+    purity=statistics/purity;
+  };
+};
+
+
+int SignalContamination(TString mass, TString ctau){
+
+  sample pred;
+  pred.file = new TFile("/nfs/dust/cms/user/tlenz/ANALYSIS/workdir/analysis_2015_10_11_METGt0_JetPt70_noBlinding/results/analyzer/ntuples/input_weighted/Madgraph_signal_mass_" + mass + "_ctau_" + ctau + "cm.root","READ");
+  //pred.file = new TFile("/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_12_02_METGt100_JetPt110_TrackPtGt20/results/analyzer/ntuples/input_weighted/Madgraph_signal_mass_100_ctau_100cm.root","READ");
+  pred.file   -> GetObject("chiTrackspreselectionTrigger/Variables",pred.tree);
+
+
+  pred.getTreeVariables();
+
+  int nbins=15;
+  double xmin=-1;
+  double xmax=2;
+
+  pred.histo = new TH1D("CR","CR",nbins,xmin,xmax);
+
+  pred.Selection();
+  cout<<"integral = "<<pred.histo->Integral()<<endl;
+
+  return 0;
+ 
+}
+
+//################################################################################################################################
+//################################################################################################################################
+//################################################################################################################################
+
+//################################################################################################################################
